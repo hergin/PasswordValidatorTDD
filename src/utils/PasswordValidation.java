54ee3b2f3e0f9cb4e1399e00caecd10d88685ec2
Slamduncan <duncan.klemm141@gmail.com>
2020-01-17 13:19:39 -0500

    Added test and code to pass test
    Added special character test
    added code to pass test

--- src/utils/PasswordValidation.java
+++ src/utils/PasswordValidation.java
@@ -2,12 +2,15 @@ package utils;
 
 public class PasswordValidation {
     public static boolean validate(String password) {
-        if (password.length() < 5 || password.length() > 15)
+        if (password.length() < 5 || password.length() > 15) {
             return false;
-
-        if (!password.chars().anyMatch(c -> Character.isUpperCase(c)))
+        }
+        if (!password.chars().anyMatch(c -> Character.isUpperCase(c))) {
             return false;
-
+        }
+        if (!(password.contains("?") || password.contains("!") || password.contains("$") || password.contains("@") || password.contains("%"))) {
+            return false;
+        }
         return true;
     }
 }
