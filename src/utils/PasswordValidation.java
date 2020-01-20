d9a0b851ae708321c6cefb7bd1d3a4bc69959aba
rosseffinger <reffinger17@gmail.com>
2020-01-19 21:13:25 -0500

    added a check for special characters.

--- src/utils/PasswordValidation.java
+++ src/utils/PasswordValidation.java
@@ -1,11 +1,18 @@
 package utils;
+import java.util.*;
+import java.util.function.IntPredicate;
 
 public class PasswordValidation {
+
     public static boolean validate(String password) {
+
         if (password.length() < 5 || password.length() > 15)
             return false;
 
         if (!password.chars().anyMatch(c -> Character.isUpperCase(c)))
+            return false;
+
+        if (!password.chars().anyMatch(c -> !Character.isAlphabetic(c)))
             return false;
 
         return true;
